c3c16669ded6d037b0735fe1f2eba5e80b434545
Ahmed Gamal <31899634+AhmedGamal7@users.noreply.github.com>
2024-05-17 12:11:26 +0300

    Update text_from_image.py

--- text_from_image.py
+++ text_from_image.py
@@ -6,14 +6,18 @@ def main():
     st.title("Image to Text Converter")
 
     uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-    language = st.selectbox("Language to convert", [
-                            "English","Arabic"])
+    language_mapping = {
+        "English": "eng",
+        "Arabic": "ara"
+    }
+    selected_language = st.selectbox("Language to convert", list(language_mapping.keys()))
+
     if uploaded_image is not None:
         image = Image.open(uploaded_image)
         st.image(image, caption='Uploaded Image', use_column_width=True)
         
         if st.button("Extract Text"):
-            text = pytesseract.image_to_string(image,lang=language)
+            text = pytesseract.image_to_string(image, lang=language_mapping[selected_language])
             st.header("Extracted Text")
             st.write(text)
 
